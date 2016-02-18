155fcf73e2bb870ef0966dc92e8a5812c2a0ac19
Luke Askew <luke@dynamit.com>
2016-02-18 17:04:30 -0500

    refactor and add some more groundwork

--- dribbble-feed/src/assets/scripts/main.js
+++ dribbble-feed/src/assets/scripts/main.js
@@ -3,8 +3,24 @@
  * @author ?
  */
 
-// button to hook click event into
-const button = document.querySelector('#load-shots');
+// Dribbble API info
+const API_ENDPOINT = 'https://api.dribbble.com/v1/shots/';
+const API_TOKEN = '';
+
+// DOM nodes
+const DOM = {
+
+	// main container
+	mainContainer: document.querySelector('[role="main"]'),
+
+	// button to hook click event into
+	button: document.querySelector('#load-shots'),
+
+	// container for the templated HTML
+	feedContainer: document.querySelector('#dribbble-feed')
+
+};
+
 
 /**
  * Return a templated feed
@@ -19,6 +35,6 @@ function templateFeed(data = {}) {
 	// template using data; return a string of HTML
 	return template(data);
 
-};
+}
 
 console.log(templateFeed());
